5329de3048576e7a3bbd70323d843bcb2eee964c
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-12-15 19:38:35 +0900

    The main .py script for crawling. It can 'visit' all of the car's pages. Now I have to consider which data to take, and its consistency of appearances (if data is not present, it might crash??)

---
+++ crawler.py
@@ -0,0 +1,49 @@
+# Import Modules
+import re # Regular Expression (Regex)
+from time import sleep
+import requests
+from bs4 import BeautifulSoup
+
+# Import My Modules
+from lib.my_modules import gaussian_sleep as g_sleep
+from lib.my_modules import print_progress as print_prog
+
+# gaussian_sleep用パラメータ: https://keisan.casio.com/exec/system/1180573188
+mu = 6 # 待機時間の平均
+sigma = 1 # 待機時間の標準偏差
+
+base_url = "https://rank.greeco-channel.com/access/?pg="
+
+# Requests関連
+session = requests.Session() # Sessionを使う事でrequestsに継続性が保たれる(例: Cookie)
+headers = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+}
+session.headers.update(headers)
+
+# 最初のページにアクセス
+c = session.get(base_url).content # 変数名: c = content
+soup = BeautifulSoup(c, 'html.parser') # Soupを作る
+
+# 最初のページからこれから取得するデータの概要を取得する
+s = soup.find("div", class_="theContentWrap-ccc").find_all("p")[4].text # tag内のtextを取り出す
+total_cars = int(re.search(r"全(.*?)件", s).group(1)) # Regexを使ってDBに登録されている車の総数を取り出す
+total_pages= int(re.search(r"・(.*?)ページ中", s).group(1)) # Regexを使って総ページ数を取り出す
+print("Total Cars: {}, Total Pages: {}".format(total_cars, total_pages))
+
+for page in list(range(1, total_pages+1))[:3]:
+    url = base_url+str(page)
+    c = session.get(url).content
+    soup = BeautifulSoup(c, 'html.parser')
+    urls = soup.find("table",class_="rank_tbl").find_all("a")[::2] # [::2]で２ステップにしている(謎の重複しているaタグがあるから)
+    urls = [url["href"] for url in urls]
+    for car_url in urls:
+        c = session.get(car_url).content
+        soup = BeautifulSoup(c, 'html.parser')
+        ######################################################
+        # Per page scraping
+        ######################################################
+        print(soup.find("title").text)
+        g_sleep(mu,sigma)
+    print_prog(page, total_pages)
+    g_sleep(mu, sigma)
