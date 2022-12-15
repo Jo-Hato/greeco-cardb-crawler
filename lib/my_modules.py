611b13d989fe26edeaf0e678cb642722939fba64
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-12-15 19:39:52 +0900

    Added /lib/my_modules.py which I shove dirty codes into. (We've all shoved our toys under our beds, haven't we?)

---
+++ lib/my_modules.py
@@ -0,0 +1,12 @@
+# We've all shoved our toys under our beds, haven't we?
+
+import time
+import random
+
+def gaussian_sleep(mu, sigma):
+    wait_sec = random.gauss(mu, sigma)
+    wait_sec = mu if (wait_sec <= 0) else wait_sec
+    return time.sleep(wait_sec)
+
+def print_progress(current, total):
+    print(f"Progress: {current}/{total} ({current/total*100:.2f}%)")
